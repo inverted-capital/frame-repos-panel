81b626a90e8ebb11e8ccabf105103e3ea48d5a1a
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-17 04:01:13 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ReposView from './components/ReposView'
 
 export default function App() {
   return (
-    <div className="p-4">
+    <div className="p-6">
       <ReposView />
     </div>
   )
